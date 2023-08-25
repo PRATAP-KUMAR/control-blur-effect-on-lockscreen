52d53c62aa4fee27bfb7a58aebdcd11927148f3f
PRATAP-KUMAR <pratap@fastmail.fm>
2023-08-25 22:21:47 +0530

    Update

--- extension.js
+++ extension.js
@@ -1,68 +1,24 @@
-// 'use strict';
-
-// import St from 'gi://St';
-// import * as UnlockDialog from 'resource:///org/gnome/shell/ui/unlockDialog.js';
-// import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-
-// let native = UnlockDialog.UnlockDialog.prototype._updateBackgroundEffects;
-
-// class ControlBlurEffectOnLockScreenExtension extends Extension {
-//     enable() {
-//         this._settings = this.getSettings();
-//         UnlockDialog.UnlockDialog.prototype._updateBackgroundEffects = this._controlBlur;
-//     }
-
-//     disable() {
-//         UnlockDialog.UnlockDialog.prototype._updateBackgroundEffects = native;
-//         // unlock-dialog is used because this extension purpose is
-//         // to control the blur effect on lock screen itself.
-//     }
-
-//     _controlBlur() {
-//         const themeContext = St.ThemeContext.get_for_stage(global.stage);
-
-//         let BRIGHTNESS_VALUE = this._settings.get_double('brightness');
-//         let SIGMA_VALUE = this._settings.get_int('sigma');
-
-//         for (const widget of this._backgroundGroup) {
-//             const effect = widget.get_effect('blur');
-
-//             if (effect) {
-//                 effect.set({
-//                     brightness: BRIGHTNESS_VALUE,
-//                     sigma: SIGMA_VALUE * themeContext.scale_factor,
-//                 });
-//             }
-//         }
-//     }
-// }
+'use strict';
 
 import {Extension, InjectionManager} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as UnlockDialog from 'resource:///org/gnome/shell/ui/unlockDialog.js';
 import St from 'gi://St';
 
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-
 export default class ControlBlurExtension extends Extension {
     constructor(metadata) {
         super(metadata);
-
         this._injectionManager = new InjectionManager();
     }
 
     enable() {
-        this._settings = this.getSettings();
-
-        // Overriding a method with an *arrow function*
         this._injectionManager.overrideMethod(UnlockDialog.UnlockDialog.prototype, '_updateBackgroundEffects',
-            originalMethod => {
-                return () => {
+            () => {
+                const settings = this.getSettings();
+                return function () {
                     const themeContext = St.ThemeContext.get_for_stage(global.stage);
 
-                    let BRIGHTNESS_VALUE = this._settings.get_double('brightness');
-                    let SIGMA_VALUE = this._settings.get_int('sigma');
-
-                    Main.notify(JSON.stringify(UnlockDialog.UnlockDialog._backgroundGroup));
+                    let BRIGHTNESS_VALUE = settings.get_double('brightness');
+                    let SIGMA_VALUE = settings.get_int('sigma');
 
                     for (const widget of this._backgroundGroup) {
                         const effect = widget.get_effect('blur');
@@ -74,13 +30,11 @@ export default class ControlBlurExtension extends Extension {
                             });
                         }
                     }
-                    originalMethod.call();
                 };
             });
     }
 
     disable() {
         this._injectionManager.clear();
-        this._settings = null;
     }
 }
--- prefs.js
+++ prefs.js
@@ -1,29 +1,111 @@
 import Adw from 'gi://Adw';
-import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
 
-import { ExtensionPreferences } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
-export default class BringoutExtensionPreferences extends ExtensionPreferences {
+export default class ControlBlurExtensionPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
+        window._settings = this.getSettings();
+
+        const adjustBlur = () => {
+            let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, margin_top: 5});
+            let blurLabel = new Gtk.Label({label: 'Adjust Sigma', xalign: 0, hexpand: true});
+
+            this.blur_adjustment = new Gtk.Adjustment({
+                lower: 0,
+                'step-increment': 1,
+                'page-increment': 10,
+                upper: 999,
+            });
+
+            this.blur_scale = new Gtk.Scale({
+                hexpand: true,
+                margin_start: 20,
+                visible: true,
+                'draw-value': true,
+                'value-pos': 'left',
+                'can-focus': true,
+                digits: 0,
+                adjustment: this.blur_adjustment,
+            });
+
+            this.resetBlurButton = new Gtk.Button({margin_start: 5});
+            this.resetBlurButton.set_label("Reset to Extensions's Default Value");
+            this.resetBlurButton.connect('clicked', () => {
+                window._settings.set_int('sigma', 0);
+                this.blur_scale.set_value(window._settings.get_int('sigma'));
+            });
+
+            this.blur_scale.set_value(window._settings.get_int('sigma'));
+            this.blur_scale.connect('value-changed', entry => {
+                window._settings.set_int('sigma', entry.get_value());
+            });
+
+            hbox.append(blurLabel);
+            hbox.append(this.blur_scale);
+            hbox.append(this.resetBlurButton);
+
+            return hbox;
+        };
+
+        const adjustBrightness = () => {
+            let hbox = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, margin_top: 5});
+            let brightnessLabel = new Gtk.Label({label: 'Adjust Brightness', xalign: 0, hexpand: true});
+
+            this.brightness_adjustment = new Gtk.Adjustment({
+                lower: 0,
+                'step-increment': 0.05,
+                'page-increment': 0.1,
+                upper: 1,
+            });
+
+            this.brightness_scale = new Gtk.Scale({
+                hexpand: true,
+                margin_start: 20,
+                visible: true,
+                'draw-value': true,
+                'value-pos': 'left',
+                'can-focus': true,
+                digits: 2,
+                adjustment: this.brightness_adjustment,
+            });
+
+            this.resetBrightnessButton = new Gtk.Button({margin_start: 5});
+            this.resetBrightnessButton.set_label("Reset to Extensions's Default Value");
+            this.resetBrightnessButton.connect('clicked', () => {
+                window._settings.set_double('brightness', 0.55);
+                this.brightness_scale.set_value(window._settings.get_double('brightness'));
+            });
+
+            this.brightness_scale.set_value(window._settings.get_double('brightness'));
+            this.brightness_scale.connect('value-changed', entry => {
+                window._settings.set_double('brightness', entry.get_value());
+            });
+
+            hbox.append(brightnessLabel);
+            hbox.append(this.brightness_scale);
+            hbox.append(this.resetBrightnessButton);
+
+            return hbox;
+        };
+
+        const addBoldText = text => {
+            let txt = new Gtk.Label({xalign: 0, margin_top: 20});
+            txt.set_markup(`<b>${text}</b>`);
+            txt.set_wrap(true);
+            return txt;
+        };
+
         const page = new Adw.PreferencesPage();
         window.add(page);
 
         const group = new Adw.PreferencesGroup();
         page.add(group);
 
-        const brightnessSlider = new Adw.SwitchRow({
-            title: 'Adjust Brightness',
-        });
-        group.add(brightnessSlider);
-
-        const sigmaSlider = new Adw.SwitchRow({
-            title: 'Adjust Sigma',
-        });
-        group.add(sigmaSlider);
-
-
-        window._settings = this.getSettings();
-        window._settings.bind('brightness', brightnessSlider, 'active', Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('sigma', sigmaSlider, 'active', Gio.SettingsBindFlags.DEFAULT);
+        group.add(adjustBlur());
+        group.add(adjustBrightness());
+        group.add(addBoldText(
+            'Please note that when Blur Sigma Value is set to 0, the Brightness will be Maximum Irrespective of the value set above.'
+        ));
     }
 }
